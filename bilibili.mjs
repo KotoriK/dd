2d58dc7dc9ad922df5f0e896460c2b69810a521a
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-10-15 22:02:26 +0800

    feat: api数据获取阶段错误处理
    feat: api数据获取阶段错误处理

--- bilibili.mjs
+++ bilibili.mjs
@@ -1,28 +1,36 @@
 import { getSpaceAccInfo } from 'bilibili-api'
 import fetch from 'node-fetch'
 import { StateCache } from './last_state.mjs'
+import logger, { parseError } from './logger.mjs'
 
 export async function getInfo(userId) {
     const req = getSpaceAccInfo(userId)
     const resp = await fetch(req.url, {
         headers: req.options.headers,
     })
-    const {
-        data: {
-            mid,name,
-            live_room: {
-                url, cover, title, liveStatus, roundStatus, roomid, roomStatus,
+    let dataReturn
+    try {
+        dataReturn = await resp.json()
+        const {
+            data: {
+                mid,
+                name,
+                live_room: {
+                    url, cover, title, liveStatus, roundStatus, roomid, roomStatus,
+                }
             }
+        } = dataReturn
+        return {
+            name,
+            url,
+            cover,
+            title,
+            roomid,
+            mid,
+            status: (liveStatus == 1 && 'LIVE') || (roundStatus == 1 && 'ROUND') || (roomStatus == 1 && 'CLOSE') || 'END'
         }
-    } = await resp.json()
-    return {
-        name,
-        url,
-        cover,
-        title,
-        roomid,
-        mid,
-        status: (liveStatus == 1 && 'LIVE') || (roundStatus == 1 && 'ROUND') || (roomStatus == 1 && 'CLOSE') || 'END'
+    } catch (e) {
+        logger.error(`B站API数据拉取失败: ${resp.status}\nbody: ${JSON.stringify(dataReturn)}\n${parseError(e)}`)
     }
 }
 export function isStatusChanged({ mid, status }) {
